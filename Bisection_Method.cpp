1a263c945c918647d41ded0050514c52e8a4f4a2
Sabiqunnahar <sabiqunnahar65@gmail.com>
2025-02-01 00:05:45 +0600

    Update Bisection_Method.cpp

--- Bisection_Method.cpp
+++ Bisection_Method.cpp
@@ -1,40 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define EPSILON 0.01
 
-double f(double x)
-{
-    return (x*x*x) - (2 *x) - 5;
+double func(double x){
+    return x*x*x-2*x*x-5;//Equation
 }
 
-double bisection( double a,double b,double e)
-{
-
-
-    while(1)
-    {
-    double c = (a+b) / 2.0;
-        if (f(c) == 0 || abs(f(c)) <=e) return c;
-
-      if(f(c) < 0) a = c;
-      if(f(c) > 0) b = c;
-    };
+void Bisection(double a,double b){
+    if(func(a)*func(b)>=0){
+        cout<<"Incorrect Assumption";
+        return;
+    }
+    double c;
+    while(b-a>=EPSILON){
+        c=(a+b)/2;
+        if(func(c)==0.0) break;
+        else if(func(a)*func(c)<0.0) b=c;
+        else if(func(a)*func(c)>0.0) a=c;
+    }
+    cout<<"The value of root is: "<<c;
 }
 
-int main ()
+int main()
 {
-    cout << "Enter The Interval: ";
     double a,b;
-    cin >> a >> b;
-    cout << endl;
-
-    cout << "Enter The Tolerance: ";
-    double e;
-    cin >> e;
-    cout << endl;
-
-    double ans = bisection(a,b,e);
-
-    cout << fixed << setprecision(4) << ans << endl;
-
-
+    cin>>a>>b;
+    Bisection(a,b);
 }
